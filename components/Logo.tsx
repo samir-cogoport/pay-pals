0d0837309b1f66c1280b25bcb0b6db4331c50204
samir-cogoport <samir.dupare@cogoport.com>
2025-10-05 01:11:20 +0530

    logo

---
+++ components/Logo.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Logo = () => {
+  return (
+        <img alt="logo" width={24} height={24} src="/logo.png" />
+  )
+}
+
+export default Logo
--- ui/landing/index.tsx
+++ ui/landing/index.tsx
@@ -5,6 +5,7 @@ import {  ArrowRight, CheckCircle2 } from "lucide-react";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import Link from "next/link";
 import { benefits, features } from "./config";
+import Logo from "@/components/Logo";
 
 const LandingPage = () => {
   return (
@@ -13,7 +14,7 @@ const LandingPage = () => {
       <nav className="border-b bg-card/50 backdrop-blur-sm sticky top-0 z-50">
         <div className="container mx-auto px-4 py-3 md:py-4 flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <div className="h-7 w-7 md:h-8 md:w-8 rounded-lg bg-gradient-hero" />
+            <Logo/>
             <span className="text-lg md:text-xl font-bold text-foreground">PayPals</span>
           </div>
           <div className="flex items-center gap-2 md:gap-4">
